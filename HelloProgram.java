27d5e7e91df89ef8b1d9b840f8ffb88574eed1cc
harikrishnankc995 <hkc@cusat.ac.in>
2021-02-16 14:29:34 +0530

    Hello World Program

--- HelloProgram.java
+++ HelloProgram.java
@@ -1,8 +1,8 @@
 public class HelloProgram {
     public static void main(String[] s) {
-        System.out.println("Loop Based Execution");
+        System.out.println("Loop Based  CID CD Execution ");
 
-        for (int i=0; i<10; i++){
+        for (int i=0; i<50; i++){
             System.out.println(i);
             
         }
